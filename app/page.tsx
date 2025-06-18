24dbc982c86b587255899af500c92a2b09775d82
Ayushliiil <113621157+Ayushliiil@users.noreply.github.com>
2025-06-18 20:15:41 +0545

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import ChatUI from '../ChatUI';
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Welcome to Lawverse™</h1>
+      <ChatUI />
+    </main>
+  );
+}
