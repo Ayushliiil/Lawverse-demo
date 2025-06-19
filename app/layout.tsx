505e02efd5ff87c54ebbbba1bba13a5d312ddd56
Ayushliiil <113621157+Ayushliiil@users.noreply.github.com>
2025-06-19 12:11:25 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <title>Lawverse™</title>
+      </head>
       <body>{children}</body>
     </html>
   );
